681d1afe5c4063c8ce060cff70c019cc453574fa
aidilrx04 <mrcrayv14@gmail.com>
2025-01-02 01:42:06 +0800

    display minimalist pie chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:duitgone2/models/money.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -21,7 +24,13 @@ class MyApp extends StatelessWidget {
             SizedBox(
               height: 300,
               width: double.infinity,
-              child: Placeholder(),
+              child: PieChart(
+                duration: Duration(milliseconds: 250),
+                PieChartData(
+                  sections: _createSections(moneys),
+                  centerSpaceRadius: 0,
+                ),
+              ),
             ),
             SizedBox(
               height: 10,
@@ -44,7 +53,7 @@ class MyApp extends StatelessWidget {
                           DateFormat("hh:mm a").format(money.date),
                         ),
                         trailing: Text(money.amount.toStringAsFixed(2)),
-                      )
+                      ),
                   ],
                 ),
               ),
@@ -53,5 +62,40 @@ class MyApp extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  List<PieChartSectionData> _createSections(List<Money> moneys) {
+    const colors = [
+      Colors.red,
+      Colors.pink,
+      Colors.purple,
+      Colors.deepPurple,
+      Colors.indigo,
+      Colors.blue,
+      Colors.lightBlue,
+      Colors.cyan,
+      Colors.teal,
+      Colors.green,
+      Colors.lightGreen,
+      Colors.lime,
+      Colors.yellow,
+      Colors.amber,
+      Colors.orange,
+      Colors.deepOrange,
+      Colors.brown,
+      Colors.grey,
+      Colors.blueGrey,
+      Colors.black,
+      Colors.white,
+    ];
+    return moneys.map((money) {
+      return PieChartSectionData(
+        value: money.amount.abs(),
+        title: money.category,
+        titlePositionPercentageOffset: 1,
+        color: colors[Random().nextInt(colors.length)],
+        radius: 100,
+      );
+    }).toList();
   }
 }
