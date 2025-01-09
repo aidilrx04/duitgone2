66de494be10eebecf1880e03bed63ee58400fe81
aidilrx04 <mrcrayv14@gmail.com>
2025-01-09 16:26:10 +0800

    feat(helpers): add method to read files in a directory

--- lib/helpers/local_storage/local_storage.dart
+++ lib/helpers/local_storage/local_storage.dart
@@ -4,5 +4,18 @@ export 'empty_local_storage.dart'
 
 abstract class AbstractLocalStorage {
   Future<String?> read(String path);
+
+  /// Read files in a directory(concept)
+  ///
+  /// On mobile, this method will return lists of filenames in the [path]
+  ///
+  /// On web, this method will return lists of matching keys like
+  /// ```dart
+  /// var final = LocalStorage().readFilesInDirectory("transactions");
+  /// ```
+  ///
+  /// the method will look for keys starting with "transactions/"
+  /// and return the string after the "/"
+  Future<List<String>> readFilesInDirectory(String path);
   Future<bool> write(String path, String content);
 }
